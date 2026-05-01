d6d6d0311cd0f8960fa5f9c909a5084643570796
joaopavan-ort <joao.pavan@ort.org.br>
2026-05-01 02:37:56 -0300

    Co-authored-by: MarceloGeschwind <MarceloGeschwind@users.noreply.github.com>

--- RPG.py
+++ RPG.py
@@ -1,23 +1,45 @@
 # Importando Biblioteca
 import random
 
-# Dado de Testes
-d20 = random.randint(1, 20)
+#
 
+# Dado de Testes
+def teste(acao, defesa, dado):
+    d20 = random.randint(1, 20)
+    if acao.lower() == 'ataque' or acao.lower() == 'especial':
+        if d20 > defesa:
+            dano = random.randint(1, dado)
+    elif acao.lower() == 'cura':
+        if d20 > 12:
+            pv += 5
+
+       
 # Criação de personagem
 input('Bem vindo ao **nome**, um RPG baseado no sistema "Ordem Paranormal". Clique enter para começar a criação de personagem.')
 nome = input('Qual o seu nome? ')
 
 while True:
     classe = input('Entre Ocultista (+PE), Combatente (+PV) e Especialista (Armas Táticas), qual é sua classe? ')
-    if classe.upper() == 'OCULTISTA' or classe.upper() == 'COMBATENTE' or classe.upper() == 'ESPECIALISTA':
+    classe = classe.lower()
+    if classe == 'ocultista' or classe == 'combatente' or classe == 'especialista':
         break
 
 Vig, For, Int, Agi, Pre = map(int, input('Agora é hora de distribuir seus 4 pontos de atributo entre Vigor (PV), Força (Dano), Intelecto (Dano Mágico), Agilidade (Dano Ágil) e Presença (PE) lembre-se que o máximo de cada atributo é 3. ').split())
 
 while True:
     arma = input('Entre Adaga (Arma Ágil), Espada (Arma Comum) e Martelo (arma tática), qual é sua arma? ')
-    if arma.upper() == 'ADAGA' or arma.upper() == 'ESPADA' or arma.upper() == 'MARTELO':
+    arma = arma.lower()
+    if arma == 'adaga':
+        dado = 4
         break
+    elif arma == 'espada':
+        dado = 6
+        break
+    elif arma == 'martelo':
+        if classe == 'especialista':
+            dado = 12
+            break
+        else:
+            print('Você não tem proficiência em armas táticas, não pode usar um martelo.')
 
 
